752a15e4b7604ba9a648a69e84e8294db14bf013
jiwoo hong <jwhong48@gmail.com>
2024-08-20 23:39:12 +0900

    first commit

---
+++ Separate.py
@@ -0,0 +1,10 @@
+# 짝수 홀수 구분
+def is_evenly_divisible(number):
+
+
+#test
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
+print(is_evenly_divisible(218))
+print(is_evenly_divisible(317))
